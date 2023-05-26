3e33e97a13e9948b28e31e65c53f0b7d2e8d7d7e
Ezrie Madison <iezrie@gmail.com>
2023-05-26 14:08:47 -0500

    5-26-23

---
+++ modern/script.js
@@ -0,0 +1,22 @@
+console.log(100);
+
+console.log('Hello Neo');
+
+console.log(20, 'Hello World');
+
+const x = 100;
+
+console.log(x);
+
+console.error('Do andriods dream?');
+
+console.table({name: 'Neo', email: 'neo@gmail.com'});
+
+console.group('simple');
+console.log(x);
+console.error('ALERT');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+console.log('%cHello Neo', styles); 
