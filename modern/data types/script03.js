2b3c460df033bf0a3f49c5184962878ab0c37540
Ezrie Madison <iezrie@gmail.com>
2023-06-01 15:28:43 -0500

    6-1-23

---
+++ modern/data types/script03.js
@@ -0,0 +1,39 @@
+// string
+const firstName = 'Sara';
+
+// number
+const age = 30;
+const temp = 98.9;
+
+// boolean
+const hasKids = true;
+
+// null
+const aptNumber = null;
+
+// undefined
+
+//let score;
+const score = undefined;
+
+// symbol
+const id = Symbol('id');
+
+// big int
+const n = 900000000000000000n;
+
+// ref types
+const numbers = [1, 2, 3, 4];
+
+const person = {
+  name: 'Brad',
+};
+
+function sayHello() {
+  console.log('Hello');
+}
+
+const output = sayHello ;
+
+console.log(output, typeof output);
+
