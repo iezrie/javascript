3d51fb61d5df5dde5757f977b2d399b4ddfb56f7
Ezrie Madison <iezrie@gmail.com>
2023-06-02 15:44:48 -0500

    6-2-23

---
+++ modern/stack-heap/script04.js
@@ -0,0 +1,11 @@
+// values stored on the stack
+const name = 'John';
+const age = 30;
+
+// reference values stored on the heap
+const person = {
+  name: 'Brad',
+  age
+
+}
+// https://www.udemy.com/course/modern-javascript-from-the-beginning/learn/lecture/37191180#overview
