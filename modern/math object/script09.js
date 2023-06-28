5c78dc55d735ab9e946015c5af2b61fff5282460
Ezrie Madison <iezrie@gmail.com>
2023-06-28 16:08:44 -0500

    6-28-23

---
+++ modern/math object/script09.js
@@ -0,0 +1,16 @@
+console.log(Math);
+
+let x;
+x = Math.sqrt(9);
+x = Math.abs(-5);
+x = Math.round(4.2);
+x = Math.ceil(4.2);
+x = Math.floor(4.9);
+x = Math.pow(2, 3);
+x = Math.min(4,5,3);
+x = Math.max(4, 5, 3); 
+x = Math.random();
+
+let y = Math.random() * 10;
+
+console.log(x);
